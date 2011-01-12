aaccba2c59b72905f875e3872a6574976603cf52
Yerlan Kusainov <lagox.mail@gmail.com>
2011-01-12 12:09:00 +0600

    add static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minus::Application.config.secret_token = 'b2c1e1685b04eb8556067efeea74939f5bb02cf5e07c0623dcea0973560275daca4a2f7c5c3a4a4df0bcaa765970d41f6843ceb423295290d62c78046288609b'
