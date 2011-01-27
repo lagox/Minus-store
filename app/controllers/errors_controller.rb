836b628388162ddd5820ea8a40fa92f15d59aebf
Yerlan Kusainov <lagox.mail@gmail.com>
2011-01-27 21:38:59 +0600

    add 404

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,7 @@
+#encoding:utf-8
+
+class ErrorsController < ApplicationController
+  def routing
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -89,4 +89,7 @@ Minus::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  
+  #404
+  match '*a' => 'errors#routing'
 end
