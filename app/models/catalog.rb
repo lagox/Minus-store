69e01a76f35de3c43608a96b9547545ccb66d4b3
lagox <lagox.mail@gmail.com>
2011-02-19 22:00:25 +0600

    Edit paperclip

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -4,14 +4,14 @@ class Catalog < ActiveRecord::Base
   #paperclip minus
   has_attached_file :minus,
                     :url => ":attachment/:id/:basename.:extension",
-                    :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+                    :path => ":rails_root/public/system/:attachment/:id/:basename.:extension"
   validates_attachment_presence :minus
   validates_attachment_content_type :minus, :content_type => ['audio/mp3', 'audio/mpeg', 'audio/x-mp3']
   
   #paperclip archive
   has_attached_file :archive,
                     :url => ":attachment/:id/:basename.:extension",
-                    :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+                    :path => ":rails_root/public/system/:attachment/:id/:basename.:extension"
   validates_attachment_presence :archive
   validates_attachment_content_type :archive, :content_type => ['application/zip','application/x-rar-compressed']
   
