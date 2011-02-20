068395e3e7ccbe8525a762ed583b28ae4c299b8d
lagox <lagox.mail@gmail.com>
2011-02-20 20:32:24 +0600

    sold attribute change

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -6,7 +6,8 @@ class PaymentController < ApplicationController
       @title = "Спасибо за покупку!"
       flash[:notice] = "Поздравляем с покупкой!"
       id = params[:ik_payment_id]
-      @sold_minus = Catalog.find(id)  
+      @sold_minus = Catalog.find(id)
+      @sold_minus.update_attribute(:sold,1)
     else
       flash[:notice] = "Оплатите покупку!"
       redirect_to store_path
